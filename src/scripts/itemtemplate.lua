36ba1c58e7e5dc80f9a8ad4cb69f33a6ac8c1cae
cable2999 <cable2999@users.noreply.github.com>
2026-01-01 20:52:57 -0600

    Some more itemtemplate stuff

--- src/scripts/itemtemplate.lua
+++ src/scripts/itemtemplate.lua
@@ -18,8 +18,8 @@ string: item.weapon_damage_type -- Derived from weapon_attack
 string: item.weapon_damdice
 number: item.weapon_avg
 
-string: item.wear_flags  -- This isn't always true.  Depends on item type.
-                         -- Can capture this by capturing wear strings into table.
+string: item.wear_flags     -- This isn't always true.  Depends on item type.
+                            -- Can capture this by capturing wear strings into table.
 
 string: item.material
 number: item.weight
@@ -27,13 +27,17 @@ number: item.weight
 string: item.armor_class
 
 string: item.max_charges
-string: item.cur_charges  -- Not sure this is worth storing.
-table:  item.spells
-number: item.spells_level
+string: item.cur_charges    -- Not sure this is worth storing.
+table:  item.spells         -- This is a simple list (order shouldn't matter)
+number: item.spells_level   
 
-table:  item.affects
-table:  item.flags  -- Incomplete list
+table:  item.affects        -- Contains key (affects string) value (number) pairs
+                            -- i.e.   affects = { dexterity = -1, hp = 10, moves = -20 }
+table:  item.flags          -- This is a simple list (order shouldn't matter)
+                            -- Incomplete list
 
+
+From other data:
 
 string: item.area_name  -- From itemsearch or map.currentArea or manual
 string: item.rarity     -- From itemsearch or legendary awareness or manual
