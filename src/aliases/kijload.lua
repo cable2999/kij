137b40837417d530454dddd7ea7f978a216e1ccb
cable2999 <cable2999@users.noreply.github.com>
2026-01-03 22:37:30 -0600

    Replacing with function call.

--- src/aliases/kijload.lua
+++ src/aliases/kijload.lua
@@ -1,15 +1,1 @@
-local profiledir = getMudletHomeDir()
-local path = profiledir .. "/kmapdata"
-
-local file_path = path.. "/kitemdb.json"
-
-
-local itemtable = loadEqData(file_path)
-
-if table.size(itemtable) >= table.size(carrion_items) then
-  cecho("Loading ".. table.size(itemtable) .." into table carrion_items.")
-  carrion_items = loadEqData(file_path)
-else
-  cecho("File version has fewer items.  Aborting...")
-end
-
+kijload()
