81f4eb4ed44a75e202ac0b8f03eb05c304666edd
cable2999 <cable2999@users.noreply.github.com>
2026-01-05 23:54:16 -0600

    Making kij save a function
    In preparation for autosaving.

--- src/aliases/kijsave.lua
+++ src/aliases/kijsave.lua
@@ -1,10 +1,1 @@
-local profiledir = getMudletHomeDir()
-local path = profiledir .. "/kmapdata"
-
-if not io.exists(path) then lfs.mkdir(path) end
-
-
-file_path = path.. "/kitemdb.json"
-
-
-saveEqData(file_path, carrion_items)
+kijsave()
--- src/scripts/initialize.lua
+++ src/scripts/initialize.lua
@@ -1,3 +1,16 @@
+function kijsave()
+    
+    local profiledir = getMudletHomeDir()
+    local path = profiledir .. "/kmapdata"
+
+    if not io.exists(path) then lfs.mkdir(path) end
+
+    file_path = path.. "/kitemdb.json"
+
+    saveEqData(file_path, carrion_items)
+   
+end
+
 function kijload()
 
     carrion_items = carrion_items or {}
